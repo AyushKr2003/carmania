5178d99861b3ac70fd3beb5177a937969c2c497f
Ayush <ayushkrsngh2003@gmail.com>
2023-04-23 19:32:49 +0530

    Main page edited

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:carmania/Auth%20Page/auth_page.dart';
 import 'package:carmania/home_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -16,7 +14,7 @@ Future<void> main() async {
     MaterialApp(
       theme: ThemeData(fontFamily: "Geomanist"),
       debugShowCheckedModeBanner: false,
-      home: (FirebaseAuth.instance.currentUser != null) ? Home_Sceen() : AuthPage(),
+      home: Home_Sceen()
     ),
   );
 }
